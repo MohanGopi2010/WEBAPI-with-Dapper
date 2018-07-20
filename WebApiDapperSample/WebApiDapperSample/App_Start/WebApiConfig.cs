ad1d18cbd0215b35c6e21e35ff5255e5074b2a2c
mohan@hirecraft.in <mohan@hirecraft.in>
2018-07-20 18:43:36 +0530

    init comit

---
+++ WebApiDapperSample/WebApiDapperSample/App_Start/WebApiConfig.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiDapperSample
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            // Intermediate json converter. 
+            var json = config.Formatters.JsonFormatter;
+
+            //Preparing JSON Settings.
+            JsonSerializerSettings jSettings = new Newtonsoft.Json.JsonSerializerSettings()
+            {
+                //JSON does not need to be neat. It'll increase the whitespace & increase the size of the RESPONSE.
+                Formatting = Formatting.None
+            };
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
+            // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
+            // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
+            //config.EnableQuerySupport();
+
+            // To disable tracing in your application, please comment out or remove the following line of code
+            // For more information, refer to: http://www.asp.net/web-api
+            config.EnableSystemDiagnosticsTracing();
+        }
+    }
+}
---
+++ WebApiDapperSample/WebApiDapperSample/Controllers/StudentController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dapper;
+
+namespace WebApiDapperSample.Controllers
+{
+    public class StudentController : ApiController
+    {
+
+        private IDbConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString);
+
+
+        [HttpGet] 
+        // GET api/values
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                List<clsStudents> _getAllStudents = dbConnection.Query<clsStudents>("select RID,Firstname, Lastname, MobileNo FROM TBL_STUDENTS WITH(NOLOCK)").ToList();
+
+                return Request.CreateResponse(HttpStatusCode.OK, _getAllStudents);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error in delete:" + ex.Message);
+            }
+        }
+
+        [HttpGet]
+        // GET api/values/5
+        public HttpResponseMessage Get(long id)
+        {
+            try
+            {
+                clsStudents _getByStudents = dbConnection.Query<clsStudents>("select RID,Firstname, Lastname, MobileNo FROM TBL_STUDENTS  WITH(NOLOCK) WHERE RID =" + id).SingleOrDefault();
+
+                return Request.CreateResponse(HttpStatusCode.OK, _getByStudents);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error in delete:" + ex.Message);
+            }
+
+
+        }
+
+        [HttpPost]
+        // POST api/values
+        public HttpResponseMessage Post([FromBody]clsStudents _students)
+        {
+            try
+            {
+                if (_students == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid input.");
+                }
+                else
+                {
+                    int rowsAffected = dbConnection.Execute(@"INSERT TBL_STUDENTS(Firstname,LastName,MobileNo) values (@FN, @LN, @MobileNo)", new { FN = _students.FirstName, LN = _students.LastName, MobileNo = _students.MobileNo });
+
+                    if (rowsAffected > 0)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.Created, "Values are created.");
+                    }
+                    else return Request.CreateResponse(HttpStatusCode.NotImplemented, "Values are created.");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error in Create:" + ex.Message);
+            }
+
+        }
+
+        [HttpPut]
+        // PUT api/values/5
+        public HttpResponseMessage Put(long id, [FromBody]clsStudents _students)
+        {
+            try
+            {
+                if (_students == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid input.");
+                }
+                else
+                {
+                    int rowsAffected = dbConnection.Execute(@"Update TBL_STUDENTS set FirstName=@FN, LastName=@LN, MobileNo=@MBN where RID= " +id, new { FN = _students.FirstName, LN = _students.LastName, MBN = _students.MobileNo });
+
+                    if (rowsAffected > 0)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, "Values are updates.");
+                    }
+                    else return Request.CreateResponse(HttpStatusCode.NotModified, "Values are not Modified.");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error in Update:" + ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        // DELETE api/values/5
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                int rowsAffected = dbConnection.Execute(@"Delete from TBL_STUDENTS  where RID= " + id);
+
+                if (rowsAffected > 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, "Values are deleted.");
+                }
+                else return Request.CreateResponse(HttpStatusCode.NotModified, "Values are not deleted.");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error in delete:" + ex.Message);
+            }
+
+        }
+   
+    }
+
+
+
+    public class clsStudents
+    {
+        public Int64 RID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNo { get; set; }
+    }
+
+}
